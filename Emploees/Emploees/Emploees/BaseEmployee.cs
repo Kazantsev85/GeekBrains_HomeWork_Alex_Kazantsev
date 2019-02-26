9b4a0476578a72633c0c2bbe30570d0fb2233a95
Kazantsev85 <kazantsev_85@mail.ru>
2019-02-26 04:54:36 +0300

    1. Созданы три класса сотрудников, базовый и два наследуемых. 2. Программа заполняет массив сотрудников, рассчитывает месячную зарплату в зависимости от типа оплаты и выводит ее на экран.

---
+++ Emploees/Emploees/Emploees/BaseEmployee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    abstract class BaseEmployee
+    {
+        public double Fee { get; set; }
+
+        public BaseEmployee (double fee)
+        {
+            Fee = fee;
+        }        
+        public abstract double Salary();
+        public abstract void Print();
+    }
+}
---
+++ Emploees/Emploees/Emploees/HourRateEmp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    class HourRateEmp : BaseEmployee
+    {
+        protected double S { get; set; }        
+        public HourRateEmp(double fee) : base(fee)
+        {
+            S = fee;
+        }
+        public override double Salary()
+        {
+            double Sal = 20.8*8*S;
+            return Sal;
+        }
+        public override void Print()
+        {
+            double Sal = Salary();
+            Console.WriteLine("Почасовая оплата: {0:f}\n", Sal);
+        }
+    }
+}
