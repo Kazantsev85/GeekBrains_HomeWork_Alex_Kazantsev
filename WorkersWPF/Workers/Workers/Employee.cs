18beedee34e6f87b1e88a65cc1df3cf39d1c85e9
Kazantsev85 <kazantsev_85@mail.ru>
2019-03-08 00:53:12 +0300

    1. Создан проект WPF 2. Соданы классы Department и Employee

---
+++ WorkersWPF/Workers/Workers/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workers
+{
+    class Employee
+    {
+        public string Department { get; set; }
+        public string Name { get; set; }
+        public string Occupation { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+
+        public Employee(string department, string name, string occupation, int age, int salary)
+        {
+            Department = department;
+            Name = name;
+            Occupation = occupation;
+            Age = age;
+            Salary = salary;
+        }
+    }
+}
