19c0dca8dc4e34681115e810f19cf36bf88c357a
Kazantsev85 <kazantsev_85@mail.ru>
2019-03-08 07:14:22 +0300

    Добавлена возможность считывать текстовый файл отделов и отображать его на форме

--- WorkersWPF/Workers/Workers/MainWindow.xaml.cs
+++ WorkersWPF/Workers/Workers/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Workers
 {
@@ -23,6 +24,50 @@ namespace Workers
         public MainWindow()
         {
             InitializeComponent();
+        }
+        // Нажатие кнопки NEW DEPARTMENT. Считывает данные из двух текст боксов и записывает их в текстовый файл Departments.txt
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string Name = TextBox1.Text;
+            string Location = TextBox2.Text;           
+            
+            Department Dep = new Department( Name, Location);
+            Dep.AddToDataBase();           
+
+        }
+        // Нажатие кнопки NEW EMPLOYEE. Считывает данные из текст боксов и записывает их в текстовый файл Employee.txt
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string Name = TextBox3.Text;
+            string Surname = TextBox4.Text;
+            string Department = TextBox6.Text;
+            string Occupation = TextBox5.Text;
+            int Age = int.Parse(TextBox7.Text);
+            int Salary = int.Parse(TextBox8.Text);
+
+            Employee Emp = new Employee(Department, Name, Surname, Occupation, Age, Salary);
+            Emp.AddToDataBase();            
+
+        }
+
+        public static List<string> deplist = new List<string>();
+        
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+                                                  
+
+            StreamReader DepartmentsList = new StreamReader("Departments.txt");            
+
+            while (!DepartmentsList.EndOfStream) 
+            {
+                deplist.Add(DepartmentsList.ReadLine());                
+            }
+            
+            for (var i = 0; i < deplist.Count; i++)
+            {
+                TextBlock1.Text += deplist[i]+"\n";                                
+            }           
         }
     }
 }
